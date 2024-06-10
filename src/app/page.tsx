bb6196af99d4cac4bc072c511458829e18581a66
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-10 13:37:33 +0530

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
 import { db } from "~/server/db";
 
-
+export const dynamic = "force-dynamic";
 const mockUrls = [
  
   "https://utfs.io/f/9420a772-4a3e-4b57-bbf9-ee36c0b10dd1-nrqgva.PNG",
@@ -18,6 +18,7 @@ const mockImages = mockUrls.map((url,index) => ({
 }));
 
 export default async function HomePage() {
+
   const posts = await db.query.posts.findMany();
   console.log(posts);
   return (
