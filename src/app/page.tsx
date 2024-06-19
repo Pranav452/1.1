fd41bb7a083cb462052ac852efdfbaef27e6beb9
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-19 17:44:48 +0530

    wowowow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,45 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { index } from "drizzle-orm/mysql-core";
+import Image from "next/image";
 import Link from "next/link";
-import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
- async function Images() {
- 
-  const images =await getMyImages();
-  return (
- 
-      
-      <div className="flex flex-wrap gap-4">
-    
-        {images.map((image) => (
-          <div key={image.id } className="flex w-48 flex-col">
-          
-              <img src={image.url}  />
-             <div> {image.name}</div>
-          </div>
-        ))}
-      </div>
-      
-  );
-} 
-
-
-
-
-export default async function HomePage() {
-
-
-
-return (
-  <main className="">
-  <SignedOut>
-  <div className="h-full w-full text-2xl text-center"> please sign in </div>
-  </SignedOut>
-  <SignedIn>
-    <Images />
-  </SignedIn>
+async function Images() {
+  const images = await getMyImages();
   
-    </main>
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
+          </Link>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
 }
 
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
+}
