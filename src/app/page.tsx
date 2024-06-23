5681eeeb40b403cb119129d854435957190ba068
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-23 13:18:10 +0530

    turbo-changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-          <Link href={`/img/${image.id}`}>
+        
             <Image
               src={image.url}
               style={{ objectFit: "contain" }}
@@ -21,7 +21,7 @@ async function Images() {
               height={192}
               alt={image.name}
             />
-          </Link>
+       
           <div>{image.name}</div>
         </div>
       ))}
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,10 +6,10 @@ import { auth } from "@clerk/nextjs/server";
 export async function getMyImages() {
 
 const user =auth();
-if (!user.userId) throw new Error("Unauthorized");
+if (!user.userId) throw new Error("Unauthorized");//(if u comment this line everyusers can se everyones images)
 
   const images = await db.query.images.findMany({
-    where: (model, {eq}) => eq(model.userId, user.userId),
+   where: (model, {eq}) => eq(model.userId, user.userId),//(if u comment this line everyusers can se everyones images)
     orderBy: (model, { desc }) => desc(model.id),
   });
   return images;
