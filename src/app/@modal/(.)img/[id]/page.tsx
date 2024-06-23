3aca7e4af358faf08ec102e48ec35fea3d3cecb1
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-24 00:18:02 +0530

    parralel started

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,9 +1,14 @@
+import { getImage } from "~/server/queries";
 
 
-export default function PhotoModal({
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = parseInt(photoId, 10);
+  const newImage = await getImage(idAsNumber); // Changed variable name to 'newImage'
+  return <div>
+    <img src={newImage.url} className="w-96" />
+  </div>;
 }
