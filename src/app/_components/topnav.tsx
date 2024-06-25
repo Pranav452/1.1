f2d1d65fa2a812b8878f75c5e7f651a8c01ec9b5
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-25 13:03:47 +0530

    ui changes

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,26 +1,35 @@
 "use client";
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { useRouter } from 'next/navigation';
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-import { UploadButton } from "~/utils/uploadthing";
-import { SimpleUploadButton } from './simple-upload-button';
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
-    return (
-      <nav className="flex items-center justify-between border-b p-4 text-xl font-semibold  ">
-        <div>Gallery</div>
-        <div className="flex flex-row gap-4 items-center">
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-               
-  <SimpleUploadButton />
-                <UserButton />
-            </SignedIn>
-
-            </div>
-        
-      </nav>
-    );
-  }
+  return (
+    <nav className="flex items-center justify-between border-b p-4 text-xl font-bold  ">
+      <aside className="flex items-center gap-[2px]">
+        <p className="text-3xl font-bold ">Timeless</p>
+        <img
+          alt=""
+          loading="lazy"
+          width={15}
+          height={15}
+          decoding="async"
+          data-nimg={1}
+          style={{ color: "transparent" }}
+          srcSet="/_next/image?url=%2FFuzzieLogo.png&amp;w=16&amp;q=75 1x, /_next/image?url=%2FFuzzieLogo.png&amp;w=32&amp;q=75 2x"
+          src="/_next/image?url=%2FFuzzieLogo.png&amp;w=32&amp;q=75"
+        />
+        <p className="text-3xl font-bold"> Treasures</p>
+      </aside>
+      <div className="flex flex-row gap-4 items-center">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,12 @@ export const dynamic = "force-dynamic";
 
 async function Images() {
   const images = await getMyImages();
-  
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-        <Link href={`/img/${image.id}`}>
+          <Link href={`/img/${image.id}`}>
             <Image
               src={image.url}
               style={{ objectFit: "contain" }}
@@ -21,8 +20,7 @@ async function Images() {
               height={192}
               alt={image.name}
             />
-
-</Link>
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
@@ -34,8 +32,13 @@ export default async function HomePage() {
   return (
     <main className="">
       <SignedOut>
-        <div className="h-full w-full text-center text-2xl">
-          Please sign in above
+        <div className="p-8 pt-40">
+          <div className="">
+            <div className="mt-6 h-full w-full text-center text-3xl">
+              <h1 className="font-bold text-5xl ">Please sign in above</h1>
+            </div>
+          </div>
+          <div id="modal-root"></div>
         </div>
       </SignedOut>
       <SignedIn>
