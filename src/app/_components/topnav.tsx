4ff02cf8ab79ef03886dbff96e2ace80f1d06c7d
Pranav nair <156781708+Pranav452@users.noreply.github.com>
2024-06-26 16:05:48 +0530

    Update topnav1.1.tsx

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -13,14 +13,14 @@ export function TopNav() {
       </aside>
       <div className="flex flex-row gap-4 items-center">
         <SignedOut>
-          <div className="flex flex-row gap-2">
-            <aside className="flex items-center gap-4">
-              <div className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                <span className="absolute inset-0 animate-spin bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"></span>
-                <button className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-800 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+         // <div className="flex flex-row gap-2">
+            //<aside className="flex items-center gap-4">
+              //<div className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                //<span className="absolute inset-0 animate-spin bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"></span>
+                //<button className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-800 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                 <SignInButton />
-                </button>
-              </div>
+                //</button>
+              //</div>
               
             </aside>
           </div>
